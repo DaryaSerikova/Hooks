e3d158430a9b5d9789f3e8bb5372962b1ec2cfe2
DaryaSerikova <dvserikova@gmail.com>
2022-03-21 16:02:57 +0700

    add function getPlanet for get data with help fetch, add useCallback to exit the loop

--- src/UseEffect.js
+++ src/UseEffect.js
@@ -1,10 +1,11 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect, useCallback } from "react";
 
 const App = () => {
   const [value, setValue] = useState(1);
   const [visible, setVisible] = useState(true);
 
   if (visible) {
+    console.log('value', value)
     return (
       <div>
         <button
@@ -22,75 +23,102 @@ const App = () => {
   }
 };
 
-const Notification = () => {
-  const [visible, setVisible] = useState(true);
-  useEffect(() => {
-    const timeout = setTimeout(
-      () => setVisible(false), 2500);//mount and update
-    return () => clearTimeout(timeout);//unmount
-  }, []);
+// const Notification = () => {
+//   const [visible, setVisible] = useState(true);
+//   useEffect(() => {
+//     const timeout = setTimeout(
+//       () => setVisible(false), 2500);//mount and update
+//     return () => clearTimeout(timeout);//unmount
+//   }, []);
 
-  return (
-    <div>
-      {visible && <p>Hello</p>}
-    </div>
-  );
+//   return (
+//     <div>
+//       {visible && <p>Hello</p>}
+//     </div>
+//   );
+// };
+
+const getPlanet = (id) => { //async??
+  // console.log(id);
+  return fetch(`https://swapi.dev/api/planets/${id}/`)
+  .then(res => res.json())
+  .then(data => data);
 };
 
-const usePlanetInfo = ({id}) => {
-
-  const [name, setName] = useState(null);
+const useRequest = (request) => {
+  const [dataState, setDataState] = useState(null);
+  console.log('test');
 
   useEffect(() => {
-    console.log(name, id-1);
     let cancelled = false;
 
-    fetch(`https://swapi.dev/api/planets/${id}/`)
-      .then(res => res.json())
-      .then(data => !cancelled && setName(data.name));
+    request()
+      .then(data => !cancelled && setDataState(data));
     return () => cancelled = true; //clean
-  }, [id]);
+  }, [ request ]);
 
-  return name;
+  return dataState;
+}
+
+const usePlanetInfo = (id) => {
+ const request = useCallback(() => getPlanet(id), [ id ]);
+  
+  return useRequest(request);
+
+  // const [name, setName] = useState(null);
+
+  // useEffect(() => {
+  //   console.log(name, id-1);
+  //   let cancelled = false;
+
+  //   fetch(`https://swapi.dev/api/planets/${id}/`)
+  //     .then(res => res.json())
+  //     .then(data => !cancelled && setName(data.name));
+  //   return () => cancelled = true; //clean
+  // }, [id]);
+
+  // return name;
 };
 
-const PlanetInfo = ({ id }) => {
+const PlanetInfo = ({id}) => {
 
-  const name = usePlanetInfo({id});
+  // const name = usePlanetInfo({id});
+  const data = usePlanetInfo(id);
+  // console.log(id)
 
     return (
-      <div>{id} - {name}</div>
+      <div>{id} {data && data.name}</div>
     );
 };
 
-const HookCounter = ({ value }) => {
+// const HookCounter = ({ value }) => {
 
-  useEffect(() => () => console.log('mount'), []);
+//   useEffect(() => () => console.log('mount'), []);
 
-  useEffect(() => () => console.log('update'));
+//   useEffect(() => () => console.log('update'));
 
-  useEffect(() => () => console.log('unmount'), []);
+//   useEffect(() => () => console.log('unmount'), []);
 
-  return <p> {value} </p>;
-};
+//   return <p> {value} </p>;
+// };
 
-class ClassCounter extends Component {
-  componentDidMount() {
-    console.log('class: mount');
-  }
+// class ClassCounter extends Component {
+//   componentDidMount() {
+//     console.log('class: mount');
+//   }
 
-  componentDidUpdate(props) {
-    console.log('class: unpdate');
-  }
+//   componentDidUpdate(props) {
+//     console.log('class: unpdate');
+//   }
 
-  componentWillUnmount() {
-    console.log('class: unmount');
-  }
+//   componentWillUnmount() {
+//     console.log('class: unmount');
+//   }
 
-  render() {
-    return <p>{this.props.value}</p>;
-  }
+//   render() {
+//     return <p>{this.props.value}</p>;
+//   }
 
-}
+// }
 
 export default App;
